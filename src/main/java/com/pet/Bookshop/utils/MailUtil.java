abc37b0849e1a49df31c09e277c7b07c2d9c7f63
vveselkova <vveselkova@cinimex.ru>
2024-07-02 16:04:48 +0300

    #feature/8 *refactor Использование MessageFormat для формирования строки formattedString

--- src/main/java/com/pet/Bookshop/utils/MailUtil.java
+++ src/main/java/com/pet/Bookshop/utils/MailUtil.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
 
+import java.text.MessageFormat;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -14,15 +16,16 @@ public class MailUtil {
     private final MailProperties mailProperties;
 
     public SimpleMailMessage createRegistrationMessage(SignUpDto signUpDto) {
+
         String defaultRegistrationText = mailProperties.getProperties().get("registration-text");
+        String formattedString = MessageFormat.format(defaultRegistrationText, signUpDto.getLogin());
+
         log.info("mailUtil-createSimpleMessage: preparing email for: {}", signUpDto.getEmail());
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(mailProperties.getUsername()); //от кого всегда одинаково
         message.setTo(signUpDto.getEmail());
         message.setSubject("Registration");
-        message.setText("Welcome, "
-                + signUpDto.getLogin()
-                + " " + ((signUpDto.getText()).equals("") ? defaultRegistrationText : signUpDto.getText()));
+        message.setText((signUpDto.getText()).equals("") ? formattedString : signUpDto.getText());
 
         return message;
     }
