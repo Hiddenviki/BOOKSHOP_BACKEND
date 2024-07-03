f1cb814e4718fc31924d0645d8cb42a80f06649c
vveselkova <vveselkova@cinimex.ru>
2024-07-03 17:06:00 +0300

    #feature/8 *refactor рефактор

--- src/main/java/com/pet/Bookshop/controllers/EmailController.java
+++ src/main/java/com/pet/Bookshop/controllers/EmailController.java
@@ -4,7 +4,6 @@ import com.pet.Bookshop.model.dto.EmailDto;
 import com.pet.Bookshop.service.EmailService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
